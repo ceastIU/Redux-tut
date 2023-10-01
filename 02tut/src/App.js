24bc3af06118cdc5b154d4b12d7f97c2badafda4
Chris East <chris.east95@gmail.com>
2023-10-01 14:04:11 -0400

    Finished part 2 of the tut

---
+++ 02tut/src/App.js
@@ -0,0 +1,14 @@
+import Counter from "./features/counter/Counter";
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
+
+function App() {
+  return (
+    <main className="App">
+      <AddPostForm />
+      <PostsList />
+    </main>
+  );
+}
+
+export default App;
