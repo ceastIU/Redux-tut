1a069804889177de0a2400212ec77b7bd31dd78e
Chris East <chris.east95@gmail.com>
2023-10-03 15:10:07 -0400

    Setting up part 4

---
+++ 04tut/src/features/posts/PostsList.js
@@ -0,0 +1,37 @@
+import { useSelector, useDispatch } from "react-redux";
+import { selectAllPosts, getPostsError, getPostsStatus, fetchPosts } from "./postsSlice";
+import { useEffect } from 'react';
+import PostsExcerpt from "./PostsExcerpt";
+
+const PostsList = () => {
+    const dispatch = useDispatch();
+
+    const posts = useSelector(selectAllPosts)
+    const postsStatus = useSelector(getPostsStatus)
+    const error = useSelector(getPostsError)
+
+    useEffect(() => {
+        if (postsStatus === 'idle') {
+            dispatch(fetchPosts())
+        }
+    }, [postsStatus, dispatch])
+        
+    let content;
+    if (postsStatus === 'loading') {
+        content = <p>"loading..."</p>
+    } else if (postsStatus === 'succeeded') {
+        const orderredPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date))
+        content = orderredPosts.map(post => <PostsExcerpt key={post.id} post={post} />)
+    } else if (postsStatus === 'failed') {
+        content = <p>{error}</p>
+    }
+
+    return (
+        <section>
+            <h2>Posts</h2>
+            {content}
+        </section>
+    )
+}
+
+export default PostsList
